6fe87a63f343200a0336d6df31c2619deffc8d11
langfjord <16120146+langfjord@users.noreply.github.com>
2019-06-20 10:58:59 +0200

    Update WP-Divi-Header-Menu-Inject.php

--- WP-Divi-Header-Menu-Inject.php
+++ WP-Divi-Header-Menu-Inject.php
@@ -3,18 +3,22 @@
 * Plugin Name: WP Divi Header Menu Inject
 * Description: This plugin contains all of the custom functions.
 * Author: Langfjord
-* Version: 0.1
+* Version: 0.2
 */
 
 /* PLUGIN CODE START */
 
-function mp_custom_header_above( $main_header ) {
-	$custom_header = '<header id="custom-header-above">';
-        $custom_header .= do_shortcode('[et_pb_section global_module="3066"][/et_pb_section]');
-    $custom_header .= '</header> <!-- #custom-header-above -->';
-	return $custom_header . $main_header;
+/* Top bar replacement */
+add_filter( 'et_html_top_header', 'top_header_layout' );
+function top_header_layout() {
+ return do_shortcode('[et_pb_section global_module=3066][/et_pb_section]');
 }
-add_filter( 'et_html_main_header', 'mp_custom_header_above' );
+
+/* Below menu injection */
+add_action( 'et_before_main_content', 'before_content_layout' );
+function before_content_layout() {
+ echo do_shortcode('[et_pb_section global_module=3066][/et_pb_section]');
+}
 
 /* PLUGIN CODE END */
 
@@ -23,6 +27,7 @@ add_filter( 'et_html_main_header', 'mp_custom_header_above' );
 /*
 
 https://www.elegantthemes.com/documentation/developers/hooks/divi-template-hooks/
+https://pavenum.com/en/divi-header-include-builder-layouts-using-wordpress-hooks/
 
 */
 
