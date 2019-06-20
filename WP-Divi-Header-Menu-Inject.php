d39d87d410453fb9462e9d099801443451d4bd54
langfjord <16120146+langfjord@users.noreply.github.com>
2019-06-20 10:10:49 +0200

    Update WP-Divi-Header-Menu-Inject.php

--- WP-Divi-Header-Menu-Inject.php
+++ WP-Divi-Header-Menu-Inject.php
@@ -1,1 +1,21 @@
+<?php
+/**
+* Plugin Name: Wordpress Plugin Starter
+* Description: This plugin contains all of the custom functions.
+* Author: Langfjord
+* Version: 0.1
+*/
 
+/* PLUGIN CODE START */
+
+function mp_custom_header_above( $main_header ) {
+	$custom_header = '<header id="custom-header-above">';
+        $custom_header .= do_shortcode('[et_pb_section global_module="3066"][/et_pb_section]');
+    $custom_header .= '</header> <!-- #custom-header-above -->';
+	return $custom_header . $main_header;
+}
+add_filter( 'et_html_main_header', 'mp_custom_header_above' );
+
+/* PLUGIN CODE END */
+
+?>
