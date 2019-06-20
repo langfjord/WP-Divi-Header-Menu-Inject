3364b1b7ab184fe37b98c4b51a5f8f7514cda156
langfjord <16120146+langfjord@users.noreply.github.com>
2019-06-20 11:21:47 +0200

    Update WP-Divi-Header-Menu-Inject.php

--- WP-Divi-Header-Menu-Inject.php
+++ WP-Divi-Header-Menu-Inject.php
@@ -6,18 +6,29 @@
 * Version: 0.2
 */
 
+/* SETTINGS START */
+
+// add a new option
+add_option('langfjord_moduleid', '3066');
+// get an option
+$option = get_option('langfjord_moduleid');
+
+/* SETTINGS END */
+
+
+
 /* PLUGIN CODE START */
 
 /* Top bar replacement NOT WORKING */
-add_filter( 'et_html_top_header', 'top_header_layout' );
-function top_header_layout() {
- return do_shortcode('[et_pb_section global_module=3066][/et_pb_section]');
-}
+//add_filter( 'et_html_top_header', 'top_header_layout' );
+//function top_header_layout() {
+// return do_shortcode('[et_pb_section global_module=3066][/et_pb_section]');
+//}
 
 /* Below menu injection */
 add_action( 'et_before_main_content', 'before_content_layout' );
 function before_content_layout() {
- echo do_shortcode('[et_pb_section global_module=3066][/et_pb_section]');
+ echo do_shortcode('[et_pb_section global_module='.$option.'][/et_pb_section]');
 }
 
 /* PLUGIN CODE END */
@@ -28,7 +39,7 @@ function before_content_layout() {
 
 https://www.elegantthemes.com/documentation/developers/hooks/divi-template-hooks/
 https://pavenum.com/en/divi-header-include-builder-layouts-using-wordpress-hooks/
-
+https://www.elegantthemes.com/blog/tips-tricks/17-wordpress-functions-php-file-hacks
 */
 
 
